7fb93d599ba07bd41333ded455895962a411fdfb
admin <2449402815@qq.com>
2020-08-12 15:03:31 +0800

    test

--- src/main/java/com/geleeq/program/Program.java
+++ src/main/java/com/geleeq/program/Program.java
@@ -42,5 +42,6 @@ public class Program {
         jedisPool.returnResource(subscriberJedis);
         jedisPool.returnResource(publisherJedis);
         jedisPool.close();
+        System.out.println("12432154");
     }
 }
