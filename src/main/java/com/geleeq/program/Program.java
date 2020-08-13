0759cf8f40ade7436b52c71f777128b77f053323
admin <2449402815@qq.com>
2020-08-13 14:22:47 +0800

    test1234567

--- src/main/java/com/geleeq/program/Program.java
+++ src/main/java/com/geleeq/program/Program.java
@@ -44,5 +44,6 @@ public class Program {
         jedisPool.close();
         System.out.println("bifuheuihf8ugh8");
         System.out.println("bifuheuihf8ugh8");
+        System.out.println("bifuheuihf8ugh8");
     }
 }
