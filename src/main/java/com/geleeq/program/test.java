e70c22dc645bedf66256c8fe95893f2592be0ac3
admin <2449402815@qq.com>
2020-08-13 10:28:56 +0800

    Revert "test34321"
    This reverts commit b9478e3a
    
    Signed-off-by: admin <2449402815@qq.com>

--- src/main/java/com/geleeq/program/test.java
+++ src/main/java/com/geleeq/program/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hwfiuni");
         System.out.println("hwfiuni");
+        System.out.println("hwfiuni");
     }
 }
